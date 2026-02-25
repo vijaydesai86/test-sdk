17cf2bf7ff132c987db32c4b964e83251a5da5e2
Vijaykumar Desai <vijaykumar.desai@arm.com>
2026-02-25 17:06:36 +0000

    Force yahoo-finance2 CJS bundle in web build

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['yahoo-finance2'],
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      'yahoo-finance2': 'yahoo-finance2/dist/cjs/index.js',
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
